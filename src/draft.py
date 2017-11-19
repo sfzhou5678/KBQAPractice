7559d1c264ac7859550b0e85f9e5f589bb4660bd
sfzhou567 <260174969@qq.com>
2017-11-19 17:31:42 +0800

    更新实体识别结果

--- src/draft.py
+++ src/draft.py
@@ -1,17 +1,6 @@
 import heapq
 import numpy as np
 
-datas=np.reshape(range(4*10*20),[4,10,20])
-pos=np.reshape(range(4),[4])
-
-print(datas)
-print(pos)
-
-for i in range(4):
-  p=pos[i]
-  print(datas[i,p])
-
-# print(datas[pos].shape)
-# print(np.take(datas,pos).shape)
-# 期望选出的是[4,20]
-
+str='abc'
+for char in str:
+  print(char)
